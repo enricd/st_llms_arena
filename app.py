8aee18f519c516383e4c1203e4d783c8e638c314
enricd <edomingodomenech@gmail.com>
2023-09-20 04:41:39 +0200

    fixins missing api key message

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def main():
         page_title="LLMs Arena", 
         page_icon="🕹️", 
         layout="wide",
+        initial_sidebar_state="expanded",
         menu_items={
             "About": """This is an app for the Streamlit LLMs Hackathon from Sep 23 developed by Enric Domingo.
                         Code: https://github.com/enricd/"""
@@ -63,7 +64,7 @@ def main():
 
         # TODO: add st.expander with instructions
 
-        openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
+        openai_api_key = "" #os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
         user_api_key = st.text_input("Introduce your OpenAI API Key (https://platform.openai.com/)", value=openai_api_key, type="password")
         if user_api_key != "":
             openai_api_key = user_api_key
@@ -109,37 +110,43 @@ def main():
         st.write("- The game ends when one of the snakes dies by hitting a wall or another snake or after 100 turns.")
         st.write("- The game is played in a 15x15 grid board. x is the horizontal axis and goes from 0 to 14 left to right. y is the vertical axis and goes from 0 to 14 up to down.")
 
-    cols0 = st.columns([3, 1, 3])
 
-    with cols0[0]:
-        model1 = st.selectbox("Select the LLM-1", options=AVAILABLE_MODELS, index=0)
-        encoding1 = tiktoken.encoding_for_model(model1.split(" ")[1])
-        llm1 = ChatOpenAI(temperature=llm1_temp, openai_api_key=openai_api_key, model_name=model1.split(" ")[1])
+    if openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key:
+        st.write("#")
+        st.warning("⬅️ Please introduce your OpenAI API Key to continue...")
 
-        with st.expander("Prompt for the Agent-1"):
-            prompt1 = {
-                "sys_msg": st.text_area("System Message (opt.)", value=DEFAULT_PROMPT1[0], height=120, key="prompt1_sys_msg"),
-                "human_msg": st.text_area("Human Message", value=DEFAULT_PROMPT1[1], height=120, key="prompt1_human_msg"),
-            }
+    else:
+        cols0 = st.columns([3, 1, 3])
 
-            st.write(f"Input tokens: `{len(encoding1.encode(prompt1['human_msg'] + prompt1['sys_msg']))}` + variables tokens (800 aprox.)")
+        with cols0[0]:
+            model1 = st.selectbox("Select the LLM-1", options=AVAILABLE_MODELS, index=0)
+            encoding1 = tiktoken.encoding_for_model(model1.split(" ")[1])
+            llm1 = ChatOpenAI(temperature=llm1_temp, openai_api_key=openai_api_key, model_name=model1.split(" ")[1])
+
+            with st.expander("Prompt for the Agent-1"):
+                prompt1 = {
+                    "sys_msg": st.text_area("System Message (opt.)", value=DEFAULT_PROMPT1[0], height=120, key="prompt1_sys_msg"),
+                    "human_msg": st.text_area("Human Message", value=DEFAULT_PROMPT1[1], height=120, key="prompt1_human_msg"),
+                }
+
+                st.write(f"Input tokens: `{len(encoding1.encode(prompt1['human_msg'] + prompt1['sys_msg']))}` + variables tokens (800 aprox.)")
 
 
-    with cols0[1]:
-        st.markdown("<h1 style='text-align:center'> - vs - </h1>", unsafe_allow_html=True)
+        with cols0[1]:
+            st.markdown("<h1 style='text-align:center'> - vs - </h1>", unsafe_allow_html=True)
 
-    with cols0[2]:
-        model2 = st.selectbox("Select the LLM-2", options=AVAILABLE_MODELS, index=0)
-        encoding2 = tiktoken.encoding_for_model(model2.split(" ")[1])
-        llm2 = ChatOpenAI(temperature=llm2_temp, openai_api_key=openai_api_key, model_name=model2.split(" ")[1])
+        with cols0[2]:
+            model2 = st.selectbox("Select the LLM-2", options=AVAILABLE_MODELS, index=0)
+            encoding2 = tiktoken.encoding_for_model(model2.split(" ")[1])
+            llm2 = ChatOpenAI(temperature=llm2_temp, openai_api_key=openai_api_key, model_name=model2.split(" ")[1])
 
-        with st.expander("Prompt for the Agent-2"):
-            prompt2 = {
-                "sys_msg": st.text_area("System Message (opt.)", value=DEFAULT_PROMPT2[0], height=120, key="prompt2_sys_msg"),
-                "human_msg": st.text_area("Human Message", value=DEFAULT_PROMPT2[1], height=120, key="prompt2_human_msg"),
-            }
+            with st.expander("Prompt for the Agent-2"):
+                prompt2 = {
+                    "sys_msg": st.text_area("System Message (opt.)", value=DEFAULT_PROMPT2[0], height=120, key="prompt2_sys_msg"),
+                    "human_msg": st.text_area("Human Message", value=DEFAULT_PROMPT2[1], height=120, key="prompt2_human_msg"),
+                }
 
-            st.write(f"Input tokens: `{len(encoding2.encode(prompt2['human_msg'] + prompt2['sys_msg']))}` + variables tokens (800 aprox.)")
+                st.write(f"Input tokens: `{len(encoding2.encode(prompt2['human_msg'] + prompt2['sys_msg']))}` + variables tokens (800 aprox.)")
 
 
     # --- Game display ---
@@ -149,12 +156,17 @@ def main():
     cols1 = st.columns([5, 2, 2, 5])
 
     with cols1[1]:
-        is_start = st.button("▶️ Play!", type="primary", use_container_width=True)
-        if is_start:
-            with cols1[2]:
-                is_stop = st.button("⏹️ Stop", type="secondary", use_container_width=True)
-                if is_stop:
-                    is_start = False
+        if openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key:
+            is_start = False
+            st.write("#")
+
+        else:
+            is_start = st.button("▶️ Play!", type="primary", use_container_width=True)
+            if is_start:
+                with cols1[2]:
+                    is_stop = st.button("⏹️ Stop", type="secondary", use_container_width=True)
+                    if is_stop:
+                        is_start = False
 
     cols2 = st.columns([4, 1, 4, 1, 4])
 
